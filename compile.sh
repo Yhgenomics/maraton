80e99ae3bbc788ebd3573b3738039692be5b3d8a
shuboyang <shuboyang@yhgenomics.com>
2015-10-26 13:08:36 +0800

    modify compile.sh

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@ rm -rf cmakebuild/
 CMaker project:maraton head:*.h,*.hpp src:*.cpp,*.hpp,*.cc,*.c flag:-Wall-std=c++11-pthread lib:../lib/libuv.a
 cd cmakebuild
 cmake .
-make
+make -j 4
 cp maraton ../../
 cd ../../
 chmod 777 maraton
